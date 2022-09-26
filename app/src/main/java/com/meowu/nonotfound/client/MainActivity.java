f70d06c51992062d9c50c3a24e57be227328d5e0
lihaihua <133313829@qq.com>
2022-09-26 10:50:53 +0800

    gitignore

--- app/src/main/java/com/meowu/nonotfound/client/MainActivity.java
+++ app/src/main/java/com/meowu/nonotfound/client/MainActivity.java
@@ -10,5 +10,18 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // init
+        init();
+    }
+
+    // init
+    private void init(){
+        // ARouter
+        routerInit();
+    }
+
+    private void routerInit(){
+
     }
 }
