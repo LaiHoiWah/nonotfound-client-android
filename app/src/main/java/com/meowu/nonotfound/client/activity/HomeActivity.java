7d84d76c1ce0d3d02ae0c830d3191db1de5d6f28
lihaihua <133313829@qq.com>
2022-09-27 16:57:24 +0800

    gitignore

--- app/src/main/java/com/meowu/nonotfound/client/activity/HomeActivity.java
+++ app/src/main/java/com/meowu/nonotfound/client/activity/HomeActivity.java
@@ -3,12 +3,9 @@ package com.meowu.nonotfound.client.activity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.meowu.nonotfound.client.R;
-import com.meowu.nonotfound.client.activity.fragment.HomeFragment;
 import com.meowu.nonotfound.client.commons.constants.RouterMap;
 
 @Route(path = RouterMap.HOME_ACTIVITY)
@@ -18,14 +15,5 @@ public class HomeActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        fragmentInit();
-    }
-
-    private void fragmentInit(){
-        FragmentManager     manager     = super.getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-
-        transaction.add(new HomeFragment(), RouterMap.HOME_FRAGMENT_TAG);
     }
 }
